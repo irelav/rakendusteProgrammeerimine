ae99a857031c4aa9f3d82f7050afa6a5bde2d010
Valera <irelav1@gmail.com>
2019-09-22 16:00:37 +0300

    Lesson 3: React JSX

--- src/index.jsx
+++ src/index.jsx
@@ -4,10 +4,6 @@ import ReactDOM from "react-dom";
 const root = document.getElementById("app");
 
 ReactDOM.render(
-    React.createElement(
-        "button",
-        {},
-        "I am button, Hello World!"
-    ),
+    <button>I'm button, hello world</button>,
     root
 );
