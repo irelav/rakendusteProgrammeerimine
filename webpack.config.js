2a825f4c1ad473a3592f40532eded785ed7240ca
Valera <irelav1@gmail.com>
2019-09-22 15:07:13 +0300

    Lesson 3: Babel with Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,17 @@ module.exports = {
             }
         ])
     ],
+    module: {
+        rules: [
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+            }
+          }
+        ]
+    },
     devServer: {
         contentBase: path.join(__dirname, 'dist'),
         compress: true,
