c580076ab19bcc81c72e170b52a65000bbd6ca8d
Valera <irelav1@gmail.com>
2019-11-10 20:53:46 +0200

    Lesson 8: notfound page

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom"; 
+import Header from "./components/Header.jsx";
 import HomePage from "./pages/HomePage.jsx";
 import ItemPage from "./pages/ItemPage.jsx";
-import { BrowserRouter, Route } from "react-router-dom"; 
-import Header from "./components/Header.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
 import SignupPage from "./pages/SignupPage.jsx";
 import UserPage from "./pages/UserPage.jsx";
+import NotFound from "./pages/NotFound.jsx";
 
 class App extends React.Component {
     state = {
@@ -37,24 +38,27 @@ class App extends React.Component {
                         />
                     } 
                 />
-                <Route path="/" exact component={HomePage} />
-                <Route path="/login" 
-                    exact 
-                    render={(props) => 
-                        <LoginPage 
-                            {...props} 
-                            onLogin={this.handleLogin}
-                        />
-                    }  />
-                <Route path="/signup" exact component={SignupPage} />
-                <Route 
-                    path="/users/:userId" 
-                    exact 
-                    render={(props) => {
-                        return <UserPage {...props} user={this.state.user}/>;
-                    }}
-                />
-                <Route path="/items/:itemId" exact component={ItemPage} />
+                <Switch>
+                    <Route path="/" exact component={HomePage} />
+                    <Route path="/login" 
+                        exact 
+                        render={(props) => 
+                            <LoginPage 
+                                {...props} 
+                                onLogin={this.handleLogin}
+                            />
+                        }  />
+                    <Route path="/signup" exact component={SignupPage} />
+                    <Route 
+                        path="/users/:userId" 
+                        exact 
+                        render={(props) => {
+                            return <UserPage {...props} user={this.state.user}/>;
+                        }}
+                    />
+                    <Route path="/items/:itemId" exact component={ItemPage} />
+                    <Route component={NotFound} />
+                </Switch>
             </BrowserRouter>
         );
     }
