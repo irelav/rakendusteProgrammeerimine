e518680da3fd62f633b19f4a270742a3df52f92d
Valera <irelav1@gmail.com>
2019-11-28 11:17:04 +0200

    Lesson 10: Logout Functionality

--- src/store/actions.js
+++ src/store/actions.js
@@ -9,6 +9,7 @@ export const ITEMS_FAILURE = "ITEMS_FAILURE";
 export const ITEM_ADDED = "ITEM_ADDED";
 export const ITEM_REMOVED = "ITEM_REMOVED";
 export const USER_UPDATE = "USER_UPDATE";
+export const TOKEN_UPDATE = "TOKEN_UPDATE";
 
 export const getItems = () => (dispatch, getState) => {
 
@@ -57,3 +58,8 @@ export const userUpdate = (user) => ({
     type: USER_UPDATE,
     payload: user,
 }); 
+
+export const tokenUpdate = token => ({
+    type: TOKEN_UPDATE,
+    payload: token,
+});
